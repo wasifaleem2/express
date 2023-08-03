ba243d69549daada5d23b8758ec1253311f4781c
wasif <wasif.aleem@braincrop.net>
2023-08-03 15:18:18 +0500

    seperate file for socket.io

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const socketIO = require("socket.io");
 const express = require('express')
 const app = express();
 app.use(express.json());
-const mongoose = require("mongoose");
 const databaseConnect = require("./database/index")
-const UserModel = require("./models/UserModel");
+const socketConnect = require("./utilis/Socket");
 
 //use cors
 var cors = require('cors');
@@ -19,7 +18,7 @@ const server = http.createServer(app);
 
 // creates socketIO server instance use polling for transport
 // and allow cross origin request from cors
-const io = socketIO(server, {
+global.io = socketIO(server, {
   transports: ["polling"],
   //origin * means cross-origin requests from any origin will be allowed.
   cors: {
@@ -32,38 +31,42 @@ const io = socketIO(server, {
 let connectedUsers = [];
 
 // socket io connection code with client
-io.on("connection", (socket) => {
-  const userPhone = socket.handshake.query.userPhone;
-  UserModel.findOneAndUpdate({ phone: userPhone }, { socketId: socket.id })
-  .then(()=>{
-    console.log(`socket id update for user phone ${userPhone}`)
-  })
-  .catch((err)=>{
-    console.log("error")
-  })
-  console.log("A user is connected", userPhone);
-  connectedUsers.push(socket.id);
-  // console.log("user list",connectedUsers);
-  let userSid = socket.id;
-  // sending updated list of clients to all clients use io instead of socket to send to every user
-  socket.emit("socket_id", userSid);
-  io.emit("connected_users", { connectedUsers });
-  socket.on("message", async (data) => {
-  console.log(`message from ${socket.id} to ${data.recipient} : ${data.message} @date ${data.date} ${data.time}`);
-    // sending message to all users 
-    // socket.broadcast.emit('message', data);
-    // for sending data specific client using socket id (data.receipent) coming from client
-    io.to(data.sender).emit('receive-message', data)
-    io.to(data.recipient).emit('receive-message', data)
-  });
+// io.on("connection", async (socket) => {
+//   const userPhone = socket.handshake.query.userPhone;
+//   console.log("socket id for new conn ", socket.id)
+//   console.log("userPhone on connection ", userPhone)
+//   let user = await UserModel.findOneAndUpdate({ phone: userPhone }, { socketId: socket.id })
+//   .then((res)=>{
+//     // console.log(`socket id update for user phone ${userPhone}`)
+//     console.log("user", res)
+//   })
+//   .catch((err)=>{
+//     console.log("error")
+//   })
+//   console.log("A user is connected", userPhone);
+//   connectedUsers.push(socket.id);
+//   console.log("user list",connectedUsers);
+//   // sending user his socket id
+//   socket.emit("socket_id", socket.id);
+//   // io.emit("connected_users", { connectedUsers });
+//   socket.on("message", async (data) => {
+//     console.log(`message from ${socket.id} to ${data.recipient} : ${data.messageText} @date ${data.date} ${data.time}`);
+//     let recipient = await UserModel.findOne({ phone: data.recipient });
+//     console.log("recipient", recipient)
+//     io.to(data.senderSocket).emit('receive-message', data)
+//     io.to(recipient.socketId).emit('receive-message', data)
+//   });
 
-  // when client disconnects
-  socket.on("disconnect", () => {
-    console.log(`socket ${socket.id} disconnected`);
-    connectedUsers = connectedUsers.filter((user) => user !== socket.id);
-    io.emit("connected_users", { connectedUsers });
-  });
-});
+  
+//   // when client disconnects
+//   socket.on("disconnect", () => {
+//     console.log(`socket ${socket.id} disconnected`);
+//     connectedUsers = connectedUsers.filter((user) => user !== socket.id);
+//     io.emit("connected_users", { connectedUsers });
+//   });
+// });
+
+global.io.on('connection', socketConnect)
 
 // api calling from routes
 app.use("/api", require("./routes/index.js"));
