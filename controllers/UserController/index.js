6b36508e5e8e6e8084cd6bfb0fd45e1e777e74e0
wasif <wasif.aleem@braincrop.net>
2025-02-26 12:02:32 +0500

    chore: change in structure and code for api version 2

--- controllers/UserController/index.js
+++ controllers/UserController/index.js
@@ -1,148 +1,164 @@
 const mongoose = require("mongoose");
 const UserModel = require("../../models/UserModel");
-const AuthModel = require("../../models/AuthModel")
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const AuthModel = require("../../models/AuthModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const fetchUsers = (req, res) => {
-    UserModel.find({})
-        .exec()
-        .then((userData) => {
-            console.log("data", userData);
-            res.status(200).send(userData);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).send(err);
-        });
+  UserModel.find({})
+    .exec()
+    .then((userData) => {
+      console.log("data", userData);
+      res.status(200).send(userData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send(err);
+    });
 };
 
 const searchUser = (req, res) => {
-    let search = req.query.search; // use querry of params in get request
-    console.log("search for ",search)
-    const regex = new RegExp(search, 'i');
-    UserModel.find({phone: { $regex: regex }})
-        .exec()
-        .then((userData) => {
-            console.log("data", userData);
-            res.status(200).send(userData);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).send(err);
-        });
+  let search = req.query.search;
+  console.log("search for ", search);
+  const regex = new RegExp(search, "i");
+  UserModel.find({ phone: { $regex: regex } })
+    .exec()
+    .then((userData) => {
+      console.log("data", userData);
+      res.status(200).send(userData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send(err);
+    });
 };
 
 const saveUser = async (req, res) => {
-    let ph = req.body.phone;
-    let name = req.body.name;
-    let password = req.body.password;
-    let time = new Date();
-    
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+  let ph = req.body.phone;
+  let name = req.body.name;
+  let password = req.body.password;
+  let time = new Date();
 
-    if (!ph || !password || !name) {
-        return res.status(406).json({ err: "need to enter all fields" });
-    }
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-    if (password < 5) {
-        return res.status(406).json({ err: "Password must be atleast 6 characters" });
-    }
+  if (!ph || !password || !name) {
+    return res.status(406).json({ err: "need to enter all fields" });
+  }
 
-    let user = new UserModel({ phone: ph, name: name, password:hashedPassword, time: time })
-    user.save()
-        .then(() => {
-            res.status(200).send("saved")
-        })
-        .catch((error) => {
-            res.status(500).send(error);
-        })
+  if (password < 5) {
+    return res
+      .status(406)
+      .json({ err: "Password must be atleast 6 characters" });
+  }
+
+  let user = new UserModel({
+    phone: ph,
+    name: name,
+    password: hashedPassword,
+    time: time,
+  });
+  user
+    .save()
+    .then(() => {
+      res.status(200).send("saved");
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
 };
 
 const verifyUser = async (req, res) => {
-    try{
-        let ph = req.body.phone;
-        // let password = req.body.password;
-        let password = "12345";
-        console.log("phone in verify users", ph)
-        const payload = {
-            phone:ph,
-            password: password
-        };
-        // The secret key is a string that is used to digitally sign the payload &
-        // verify its authenticity.
-        const secretKey = `my_secret_key`;
-        console.log("phone in verify", ph)
-        const user = await UserModel.findOne({phone : "333"})
-        console.log("finded user -> ", user, payload)
-        if(user != null && user != undefined)
-        {
-            const token = jwt.sign(payload, secretKey, { expiresIn: '24h' });
-            console.log("user token to send ->", token)
-            AuthModel.findOneAndUpdate(
-                { phone: ph },  // condition to check for existing document
-                { $set: {phone: ph, token: token} },  // update operation
-                { upsert: true, new: true },)
-            .then(() => {
-                req.user = payload;
-                console.log("req.user",req.user);
-                res.status(200).send({token, phone:ph});
-            })
-            .catch((error) => {
-                res.status(500).send(error);
-            })
-            // res.status(200).send(token);
-        }
-        else{
-            res.status(200).send("No User Found");
-        }
+  try {
+    let ph = req.body.phone;
+    // let password = req.body.password;
+    let password = "12345";
+    console.log("phone in verify users", ph);
+    const payload = {
+      phone: ph,
+      password: password,
+    };
+    // The secret key is a string that is used to digitally sign the payload &
+    // verify its authenticity.
+    const secretKey = `my_secret_key`;
+    console.log("phone in verify", ph);
+    const user = await UserModel.findOne({ phone: "333" });
+    console.log("finded user -> ", user, payload);
+    if (user != null && user != undefined) {
+      // const token = jwt.sign(payload, secretKey, { expiresIn: '24h' });
+      const token = jwt.sign(payload, secretKey);
+      console.log("user token to send ->", token);
+      AuthModel.findOneAndUpdate(
+        { phone: ph }, // condition to check for existing document
+        { $set: { phone: ph, token: token } }, // update operation
+        { upsert: true, new: true }
+      )
+        .then(() => {
+          req.user = payload;
+          console.log("req.user", req.user);
+          res.status(200).send({ token, phone: ph });
+        })
+        .catch((error) => {
+          res.status(500).send(error);
+        });
+      // res.status(200).send(token);
+    } else {
+      res.status(200).send("No User Found");
     }
-    catch(error){
-        res.status(500).send(error);
-    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 const updateUser = (req, res) => {
-    let ph = req.params.phone;
-    let name = req.body.name;
-    UserModel.findOneAndUpdate({ phone: ph }, { name: name })
-        .then(() => {
-            res.status(200).send(`User updatedwith phone ${ph}`);
-        })
-        .catch((error) => {
-            res.status(500).send(error);
-        })
+  let ph = req.params.phone;
+  let name = req.body.name;
+  UserModel.findOneAndUpdate({ phone: ph }, { name: name })
+    .then(() => {
+      res.status(200).send(`User updatedwith phone ${ph}`);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
 };
 
 const deleteUser = (req, res) => {
-    let ph = req.params.phone
-    UserModel.deleteOne({ phone: ph })
-        .then(() => {
-            res.status(200).send(`(${ph}) user Deleted `);
-        })
-        .catch((error) => {
-            res.status(500).send(error);
-        })
+  let ph = req.params.phone;
+  UserModel.deleteOne({ phone: ph })
+    .then(() => {
+      res.status(200).send(`(${ph}) user Deleted `);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
 };
 
 const deleteAll = (req, res) => {
-    UserModel.deleteMany({})
-        .then(() => {
-            res.status(200).send(`All user Deleted `);
-        })
-        .catch((error) => {
-            res.status(500).send(error);
-        })
+  UserModel.deleteMany({})
+    .then(() => {
+      res.status(200).send(`All user Deleted `);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
 };
 
 const revokedTokens = new Set();
 const logout = (req, res) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    // Add the token to the blacklist
-    revokedTokens.add(token);
-    res.sendStatus(200);
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  // Add the token to the blacklist
+  revokedTokens.add(token);
+  res.sendStatus(200);
 };
 
-module.exports = {fetchUsers, searchUser, verifyUser, saveUser, updateUser, deleteUser, deleteAll, logout}
+module.exports = {
+  fetchUsers,
+  searchUser,
+  verifyUser,
+  saveUser,
+  updateUser,
+  deleteUser,
+  deleteAll,
+  logout,
+};
--- utilis/Socket.js
+++ utilis/Socket.js
@@ -25,18 +25,21 @@ const socketConnect = async (socket) => {
   // sending user his socket id
   socket.emit("socket_id", socket.id);
   io.emit("connected_users", { connectedUsers });
-  // socket.on("message", async (data) => {
-  //   let newMessage = new MessageModel({senderNumber: data.senderNumber, receiverNumber: data.receiverNumber, text:data.text, date:data.date,time: data.time, messageType: data.messageType})
-  //   await newMessage.save();
-  //   // console.log(`message from ${socket.id} to ${data.recipient} : ${data.messageText} @date ${data.date} ${data.time}`);
-  //   let recipient = await UserModel.findOne({ phone: data.receiverNumber });
-  //   let sender = await UserModel.findOne({ phone: data.senderNumber });
-  //   console.log("message", data.text)
-  //   io.to(sender.socketId).emit('receive-message', data)
-  //   io.to(recipient.socketId).emit('receive-message', data)
-  // });
+  socket.on("message-read", async (data) => {
+    try {
+      const result = await MessageModel.updateMany(
+        {
+          senderNumber: data.senderNumber,
+          receiverNumber: data.receiverNumber,
+        },
+        { status: "read" }
+      );
+      console.log("Update successful:", result);
+    } catch (error) {
+      console.error("Error updating messages:", error);
+    }
+  });
 
-  // when client disconnects
   socket.on("disconnect", () => {
     console.log(`socket ${socket.id} disconnected`);
     connectedUsers = connectedUsers.filter((user) => user !== socket.id);
