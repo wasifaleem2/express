ef338c174188758d0f1eada58eff609527deb46e
wasif <wasif.aleem@braincrop.net>
2026-08-13 14:13:12 +0500

    fix: end to end encryption

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { fetchUsers, verifyUser, login, saveUser, updateUser, deleteUser, deleteAll, logout, searchUser, pushNotificationTest, registerAppToken, registerPublicKey, getPublicKey } = require("../controllers/UserController/index");
+const { fetchUsers, verifyUser, login, saveUser, updateUser, deleteUser, deleteAll, logout, searchUser, pushNotificationTest, registerAppToken, registerPublicKey, getPublicKey, changePassword } = require("../controllers/UserController/index");
 const {getMessage, getMessagedUsers, sendMessage, updateMessage, deleteMessage, deleteChat, getAllMessages, getNoOfMessage} = require("../controllers/MessagesController/index")
 //middlewares
 const authenticate = require("../middlewares/authenticate/index")
@@ -16,6 +16,7 @@ router.put(`/update/:phone`,authenticate, updateUser)
 router.delete(`/delete/:phone`,authenticate, deleteUser)
 router.post(`/logout`,authenticate, logout)
 router.post(`/app-token`,authenticate, registerAppToken)
+router.post(`/change-password`,authenticate, changePassword)
 
 // E2EE key exchange
 router.post(`/keys`, authenticate, registerPublicKey)
